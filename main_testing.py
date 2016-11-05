2ff1403b2195c1ce667f672d6759bddae047bbc6
hannahlu <HU196882@falmouth.ac.uk>
2016-11-05 22:09:18 +0000

    Committing main_testing.py to develop branch.

---
+++ main_testing.py
@@ -0,0 +1,177 @@
+import pygame, sys
+from pygame.locals import *
+
+# set up pygame
+pygame.init()
+pygame.font.init()
+
+# set up window
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 875
+
+# set up colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Load Images
+bequietImg = pygame.image.load("shh.png")
+classicImg = pygame.image.load("classic.jpg")
+kitchencatsImg = pygame.image.load('kitchencats.jpg')
+knightIcon = pygame.image.load('shh.png')
+loneknightImg = pygame.image.load("loneknight.png")
+
+# Set up fonts
+edfont = pygame.font.SysFont("impact", 72)
+edfont2 = pygame.font.SysFont("impact", 60)
+Textfont = pygame.font.SysFont("impact", 45)
+Textfont2 = pygame.font.SysFont("impact", 50)
+kitchencatsfont = pygame.font.SysFont('Roman', 90)
+
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+window.fill(WHITE)
+pygame.display.set_caption('Memes')
+clock = pygame.time.Clock()
+#PXArray = pygame.PixelArray(window)
+
+"""Ed's Lecture Meme (Press 'e' on keyboard)"""
+def edlecture():
+
+    # Meme text font, size, anti-aliasing and colour
+    Fontimg = edfont.render("When Ed's lecture is", 1, WHITE)
+    Fontimg2 = edfont2.render("9AM monday morning", 1, WHITE)
+    window.blit(classicImg, (0, 0))
+    window.blit(Fontimg, (25, 0))
+    window.blit(Fontimg2, (53, 400))
+
+"""Crusading Meme (Press 'c' on keyboard)"""
+def crusadememe():
+
+    knightIcon = pygame.image.load('knight.png')
+    pygame.display.set_icon(knightIcon)
+    pygame.display.set_caption("Take back Jerusalem")
+
+    # Meme text font, size, anti-aliasing and colour
+    Fontimg = Textfont.render("When you were supposed to go crusading", 1, WHITE)
+    Fontimg2 = Textfont2.render("but your squad ditches you", 1, WHITE)
+
+    # Loads, draws and updates the meme/display
+    window.blit(loneknightImg, (0, 0))
+    window.blit(Fontimg, (20, 10))
+    window.blit(Fontimg2, (120, 450))
+    pygame.display.update()
+
+"""Homework Meme (Press 'h' on keyboard)"""
+def homeworkmeme():
+
+    pygame.display.set_icon(knightIcon)
+    pygame.display.set_caption("pls stop")
+
+    # Meme text font, size, anti-aliasing and colour
+    Fontimg = Textfont.render("When someone is about to remind", 1, WHITE)
+    Fontimg2 = Textfont2.render("the teacher about homework", 1, WHITE)
+
+    # Loads, draws and updates the meme/display
+    window.blit(bequietImg, (0, 0))
+    window.blit(Fontimg, (11, 10))
+    window.blit(Fontimg2, (32, 415))
+
+"""Kitchen Cat Meme (Press 'k' on keyboard)"""
+def kitchencats():
+    text = kitchencatsfont.render('When a flatmate', True, WHITE)
+    text2 = kitchencatsfont.render("won't wash up.", True, WHITE)
+    window.blit(kitchencatsImg, (0, 0))
+    window.blit(text, (20, 10))
+    window.blit(text2, (60, 100))
+
+"""Make image Red (Press 'r' on keyboard)"""
+def makered():
+    for Y in range(0, WINDOW_HEIGHT):
+        for X in range(0, WINDOW_WIDTH):
+
+            RED = window.get_at((X, Y)).r
+            GREEN = window.get_at((X, Y)).g
+            BLUE = window.get_at((X, Y)).b
+
+            RED = 255
+            GREEN = 255 - GREEN
+            BLUE = 255 - BLUE
+
+            PXArray[X, Y] = (RED, GREEN, BLUE)
+
+"""Make image Green (Press 'g' on keyboard)"""
+def makegreen():
+    for Y in range(0, WINDOW_HEIGHT):
+        for X in range(0, WINDOW_WIDTH):
+            RED = window.get_at((X, Y)).r
+            GREEN = window.get_at((X, Y)).g
+            BLUE = window.get_at((X, Y)).b
+
+            RED = 255 - RED
+            GREEN = 255
+            BLUE = 255 - BLUE
+
+            PXArray[X, Y] = (RED, GREEN, BLUE)
+
+"""Make image Blue (Press 'b' on keyboard)"""
+def makeblue():
+    for Y in range(0, WINDOW_HEIGHT):
+        for X in range(0, WINDOW_WIDTH):
+            RED = window.get_at((X, Y)).r
+            GREEN = window.get_at((X, Y)).g
+            BLUE = window.get_at((X, Y)).b
+
+            RED = 255 - Red
+            GREEN = 255 - GREEN
+            BLUE = 255
+
+            PXArray[X, Y] = (RED, GREEN, BLUE)
+
+"""Convert image to negative (Press 'n' on keyboard)"""
+def negative():
+    for Y in range(0, WINDOW_HEIGHT):
+        for X in range(0, WINDOW_WIDTH):
+            RED = window.get_at((X, Y)).r
+            GREEN = window.get_at((X, Y)).g
+            BLUE = window.get_at((X, Y)).b
+
+            RED = 255 - RED
+            GREEN = 255 - GREEN
+            BLUE = 255 - BLUE
+
+            PXArray[X, Y] = (RED, GREEN, BLUE)
+
+# set up program loop
+while True:
+    pressed = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_k:
+            kitchencats()
+        if event.type == KEYDOWN and event.key == K_r:
+            makered()
+        if event.type == KEYDOWN and event.key == K_g:
+            makegreen()
+        if event.type == KEYDOWN and event.key == K_b:
+            makeblue()
+        if event.type == KEYDOWN and event.key == K_n:
+            negative()
+        if event.type == KEYDOWN and event.key == K_e:
+            edlecture()
+        if event.type == KEYDOWN and event.key == K_c:
+            crusadememe()
+        if event.type == KEYDOWN and event.key == K_h:
+            homeworkmeme()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if pressed[K_ESCAPE]:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(200)
+
+pygame.quit()
+quit()
